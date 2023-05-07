1392087876f01063ade2ee47818d2bcfdd0a76a2
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-07 22:53:50 +0300

    Delete YukkiMusic directory

--- YukkiMusic/check_client.py
+++
@@ -1,9 +0,0 @@
-
- 
-
-def _check_client(CallbackQuery):
-
-    user_id = CallbackQuery.from_user.id
-    if user_id != CallbackQuery.message.reply_to_message.from_user.id:
-        return 
-    return True
