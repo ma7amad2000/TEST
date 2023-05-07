5a06c1755490e4fd6432fae13539b3a89e99d02d
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-07 22:56:56 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, idle
+#'‹ ٰ💸 ⇣ سورس الفراعنة ⇣ 💸 › .'#
+from pyromod import listen
+
+
+
+bot = Client(
+    "mo",
+    api_id=8186557,
+    api_hash="efd77b34c69c164ce158037ff5a0d117",
+    bot_token="5912908363:AAGkAqH3VTR0d-g0lAervmElqo9TqgDYLu4",#توكن المصنع
+    plugins=dict(root="MHelal")
+    )
+
+async def start_helalbot():
+    print("تم تشغيل الصانع بنجاح..💗")
+    await bot.start()
+    hh = "MohamedHelal_l"#يوزر المطور المصنع
+    try:
+        await bot.send_message(hh, "**تم تشغيل الصانع بنجاح ...🥀**")
+    except:
+        pass
+    await idle()
